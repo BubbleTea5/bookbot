5266b750737684b75b82144f72e716480aa3e67c
BubbleTea5 <DanielW.6656@proton.me>
2025-07-23 03:35:43 -0700

    cleaned up version after looking through solutions

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-from stats import count_words, count_characters, sorted_letter_counts
+from stats import get_text, count_words, count_characters, sorted_letter_counts
 
-book_path = "books/frankenstein.txt"
-
-def report(book_path):
-    with open(book_path) as f:
-        file_contents = f.read()
-        num_words = count_words(file_contents)
-        num_chars = count_characters(file_contents)
+def main():
+        book_path = "books/frankenstein.txt"
+        text = get_text(book_path)
+        num_words = count_words(text)
+        num_chars = count_characters(text)
         sorted_list = sorted_letter_counts(num_chars)
+
         print("============ BOOKBOT ============")
         print(f"Analyzing book found at {book_path}...")
         print("----------- Word Count ----------")
         print(f"Found {num_words} total words")
         print("--------- Character Count -------")
         for letter in sorted_list:
-            character = letter["char"]
-            count = letter["num"]
-            if character.isalpha() == True:
-                print(f"{character}: {count}")
+            # you can use .isalpha() on the *value*
+            if letter["char"].isalpha():
+                print(f"{letter['char']}: {letter['num']}")
         print("============= END ===============")
-
-def main():
-    report(book_path)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+
 def count_words(text):
     words = text.split()
     return len(words)
@@ -15,17 +19,17 @@ def count_characters(text):
             letter_count[s_letter] = 1
     return letter_count
 
+# provides a "key" to the .sort() function, now the .sort() knows to sort the values by the "num" key in item_list 
 def sorted(item_list):
     return item_list["num"]
- 
+
+# takes in a dictionary with a certain letter ("char") being the *key* and number of appearances ("num") as *value*
 def sorted_letter_counts(dictionary):
     letter_dicts = []
     for entry in dictionary:
-        char_dict = {}
-        char_dict["char"] = entry
-        char_dict["num"] = dictionary[entry]
-        letter_dicts.append(char_dict)
-    
+        # adds {"char": *key*, "num": *value*} to letter_dicts
+        letter_dicts.append({"char": entry, "num": dictionary[entry]})
+    # sort() function, reverse=True means reverse order, key=sorted means it is sorting based off the sorted() function (sorting by "num" value) above
     letter_dicts.sort(reverse=True, key=sorted)
     return letter_dicts
 
